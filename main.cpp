ecfeb8c6fae645c73cfc06e041f467cd27b8def2
Ekkorus101 <Ekkoruse@outlook.com>
2023-07-21 00:59:59 +0800

    2

--- main.cpp
+++ main.cpp
@@ -4,13 +4,10 @@
  
 int main(int argc, char* argv[])
 {
- 
     // Beginning of parallel region
-    #pragma omp parallel
-    {
- 
-        printf("Hello World... from thread = %d\n",
-               omp_get_thread_num());
+    #pragma omp parallel for num_threads(8)
+    for(int i=0;i<20;i++){
+        uint8_t* t=(uint8_t* )malloc(20);
     }
     // Ending of parallel region
 }
