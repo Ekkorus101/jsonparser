dbd4fd278a786f714e3cd0d027d4adae7de3623d
Ekkorus101 <Ekkoruse@outlook.com>
2023-07-27 00:06:01 +0800

    t

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
-#include <omp.h>
+// #include <omp.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
- 
-int main(int argc, char* argv[])
-{
-    // Beginning of parallel region
-    #pragma omp parallel for num_threads(8)
-    for(int i=0;i<20;i++){
-        uint8_t* t=(uint8_t* )malloc(20);
-    }
-    // Ending of parallel region
+
+int main(int argc, char* argv[]) {
+// #pragma omp parallel
+//     { 
+//         int a=209+1;
+//     }
 }
